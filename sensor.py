40748107baf7de106a544d0fed9822eff5148bd2
Keanu Winardi <107297270+KeanuWinardi@users.noreply.github.com>
2022-07-18 19:50:12 +0700

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -5,7 +5,7 @@ sens = Adafruit_DHT.DHT11
 gpio = 4
 
 while True:
-    humi, temp = Adafruit_DHT.read_retry(sens, gpio)
+    humi, temp = Adafruit_DHT.read(sens, gpio)
     if humi is not None and temp is not None :
         print("Temperature: {0:.01f}*C   Humidity: {0:0.1f}%".format(temp,humi))
 else :
